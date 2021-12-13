df0b50505351e302948933906f8fb1bbcf868851
Charan Gautam <gautamsaicharan@yahoo.com>
2021-12-12 17:45:00 -0700

    integration testing / more test cases

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react'
+
 import './App.css';
+import './nprogress.css';
+import { getEvents, extractLocations } from './api'
 
 // embedded components
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 
-function App() {
-  return (
-    <div className="App">
-      <CitySearch />
-      <NumberOfEvents />
-      <EventList />
-    </div>
-  );
+class App extends Component {
+  state = {
+    events: [],
+    locations: []
+  }
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = (location === "all" ? events : events.filter((event) => event.location === location));
+      this.setState({
+        events: locationEvents
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      this.mounted && this.setState({ events, locations: extractLocations(events) });
+    });
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
+        <NumberOfEvents />
+        <EventList events={this.state.events} />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -3,24 +3,27 @@ import React, { Component } from 'react';
 class CitySearch extends Component {
     state = {
         query: '',
-        suggestions: []
+        suggestions: [],
+        showSuggestions: false
     }
 
     handleInputChanged = (event) => {
         const value = event.target.value;
-        // const suggestions = this.props.locations.filter((location) => {
-        //     return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
-        // });
+        const suggestions = this.props.locations.filter((location) => {
+            return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+        });
         this.setState({
             query: value,
-            // suggestions
+            suggestions
         });
     }
 
     handleItemClicked = (suggestion) => {
         this.setState({
-            query: suggestion
+            query: suggestion,
+            showSuggestions: false
         });
+        this.props.updateEvents(suggestion)
     }
 
     render() {
@@ -31,15 +34,18 @@ class CitySearch extends Component {
                     className="city"
                     value={this.state.query}
                     onChange={this.handleInputChanged}
+                    onFocus={() => this.setState({ showSuggestions: true })}
                 />
-                <ul className="suggestions">
+                <ul className="suggestions" style={this.state.showSuggestions ? {} : { display: "none" }}>
                     {this.state.suggestions.map((suggestion) => (
                         <li
                             key={suggestion}
                             onClick={() => this.handleItemClicked(suggestion)}
-                        >{suggestion}</li>
+                        >
+                            {suggestion}
+                        </li>
                     ))}
-                    <li key='all'>
+                    <li onClick={() => this.handleItemClicked("all")}>
                         <b>See all cities</b>
                     </li>
                 </ul>
