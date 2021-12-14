ab55a751a9b82beda3065e78b9ca4835315d6a69
Charan Gautam <gautamsaicharan@yahoo.com>
2021-12-13 19:05:13 -0700

    end to end testing for first feature

--- src/__test__/EndToEnd.test.js
+++ src/__test__/EndToEnd.test.js
@@ -1,6 +1,37 @@
 
 import puppeteer from 'puppeteer';
 
+describe('filter events by city', () => {
+    let browser;
+    let page;
+    beforeAll(async () => {
+        browser = await puppeteer.launch();
+        page = await browser.newPage();
+        await page.goto('http://localhost:3000/');
+        await page.waitForSelector('.event');
+    });
+
+    afterAll(() => {
+        browser.close();
+    });
+
+    test('When user hasn’t searched for a city, show upcoming events from all cities', async () => {
+        const eventList = await page.$('.event-list li');
+        expect(eventList).toBeDefined()
+    });
+
+    test('User should see a list of suggestions when they search for a city.', async () => {
+        await page.type('.city', 'Berlin')
+        const locationSuggestion = await page.$('.suggestions li');
+        expect(locationSuggestion).toBeDefined()
+    });
+
+    test('User can select a city from the suggested list.', async () => {
+        await page.type('.city', 'Berlin')
+        await page.click('.suggestions li');
+    });
+})
+
 
 describe('show/hide an event details', () => {
     let browser;
