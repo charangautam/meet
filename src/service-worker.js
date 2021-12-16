e96111fec7ed52560b292e2ee6e8e37cdb198192
Charan Gautam <gautamsaicharan@yahoo.com>
2021-12-16 14:22:01 -0700

    fetch and install handler in service-worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -70,3 +70,30 @@ self.addEventListener('message', (event) => {
 });
 
 // Any other custom service worker logic can go here.
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(async function () {
+    try {
+      var res = await fetch(event.request);
+      var cache = await caches.open('cache');
+      cache.put(event.request.url, res.clone());
+      return res;
+    }
+    catch (error) {
+      return caches.match(event.request);
+    }
+  }());
+});
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open('cache').then(function (cache) {
+      return cache.addAll([
+        "./meet/",
+        "./meet/public/index.html",
+        "./meet/src/App.css",
+        "./meet/src/App.js"
+      ]);
+    })
+  );
+});
