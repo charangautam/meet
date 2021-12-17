d9c4d448d59fa0dc421b7016ce072494783ec0cd
Charan Gautam <gautamsaicharan@yahoo.com>
2021-12-17 14:59:50 -0700

    pwa fix

--- src/service-worker.js
+++ src/service-worker.js
@@ -89,6 +89,7 @@ self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open('cache').then(function (cache) {
       return cache.addAll([
+        "./",
         "./public/index.html",
         "./src/App.css",
         "./src/App.js"
